de55a433946db0dbd159b874b2a183e37cded7bd
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2018-10-23 19:03:12 -0700

    Switch to simplifyify for bundling.

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
-import P2P from './src/P2P'
+import P2P, {EventNames} from './src/P2P'
+import register from './src/packer'
+
 export default P2P
+export {
+    EventNames,
+    register as Packer,
+}
