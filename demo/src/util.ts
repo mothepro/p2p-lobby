bf7079dfd191def777c4a556592e65c0e88deea7
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2018-11-05 03:44:18 +0100

    Fix the function to make user input safe for html

--- demo/src/util.ts
+++ demo/src/util.ts
@@ -1,6 +1,20 @@
 /* Prevent XSS */
 export function htmlSafe (str: string | void | { toString(): string }) {
-    if(str)
-        return str.toString().replace('<', '&lt;').replace('>', '&gt;')
-    return ''
+    if(!str)
+        return ''
+
+    const unsafeChars = new Map([
+        ['&', '&amp;'],
+        ['<', '&lt;'],
+        ['>', '&gt;'],
+        ['"', '&quot;'],
+        ['\'', '&#x27;'], // &apos; not recommended because its not in the HTML spec
+                          //(See: section 24.4.1) &apos; is in the XML and XHTML specs.
+        ['/', '&#x2F;'],  // forward slash is included as it helps end an HTML entity
+    ])
+
+    let ret = str.toString()
+    for(const [unsafe, safe] of unsafeChars)
+        ret = ret.replace(new RegExp(unsafe, 'g'), safe)
+    return ret
 }
